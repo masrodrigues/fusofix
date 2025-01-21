2ccc38710da1198791f587bae35b4ce7267b8542
Marco Rodrigues <84645887+masrodrigues@users.noreply.github.com>
2025-01-21 20:24:54 -0300

    CT

--- busca/import_excel.py
+++ busca/import_excel.py
@@ -13,7 +13,6 @@ django.setup()
 # Importar o modelo Produto
 from busca.models import Produto
 
-
 # Caminho do arquivo Excel
 excel_file_path = r"C:\Users\marco.rodrigues\Documents\base_belenus.xlsx"
 
@@ -22,13 +21,27 @@ df = pd.read_excel(excel_file_path)
 
 # Iterar pelos dados e inserir no banco
 for _, row in df.iterrows():
-    produto = Produto(
-        codigo_do_produto=row['codigo_do_produto'],
-        descricao=row['descricao'],
-        quantidade=row['quantidade'],
-        valor=row['valor'],
-        valor_unitario=row['valor_unitario']
-    )
-    produto.save()
+    # Verificar se o produto já existe no banco
+    produto_existente = Produto.objects.filter(codigo_do_produto=row['codigo_do_produto']).first()
+    
+    if produto_existente:
+        # Atualizar o registro existente
+        produto_existente.descricao = row['descricao']
+        produto_existente.quantidade = row['quantidade']
+        produto_existente.valor = row['valor']
+        produto_existente.valor_unitario = row['valor_unitario']
+        produto_existente.save()
+        print(f"Produto atualizado: {row['codigo_do_produto']}")
+    else:
+        # Criar um novo registro
+        produto = Produto(
+            codigo_do_produto=row['codigo_do_produto'],
+            descricao=row['descricao'],
+            quantidade=row['quantidade'],
+            valor=row['valor'],
+            valor_unitario=row['valor_unitario']
+        )
+        produto.save()
+        print(f"Produto inserido: {row['codigo_do_produto']}")
 
-print("Dados importados com sucesso!")
+print("Importação concluída!")
